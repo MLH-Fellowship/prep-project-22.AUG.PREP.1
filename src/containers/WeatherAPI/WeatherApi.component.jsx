4c03abbc99a8d1e167036f5ba14563f2126aa584
Gopi Vaibhav <gopivaibhav0@gmail.com>
2022-08-10 20:21:03 +0530

    geolocation added

--- src/containers/WeatherAPI/WeatherApi.component.jsx
+++ src/containers/WeatherAPI/WeatherApi.component.jsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const WeatherApi = () => {
+    const [latitude, setLatitude] = useState()
+    const [longitude, setLongitude] = useState()
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            setLatitude(position.coords.latitude)
+            setLongitude(position.coords.longitude)
+          });
+    }, [])
+
     return (
-        <div className="WeatherApi w-3/4 h-20 bg-gray-300 rounded-[10px] text-center p-8 mr-5">
+        <div className="WeatherApi w-3/4 h-21 bg-gray-300 rounded-[10px] text-center p-8 mr-5">
             <h2>Weather API Section</h2>
+            <p>Your Latitude is - {latitude}</p>
+            <p>Your Longitude is - {longitude}</p>
         </div>
     )
 }
