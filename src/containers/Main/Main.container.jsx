ba866bcc02e04a37599e0eb7fb1d918fd5f98a50
Joozef <m.youssef315@yahoo.com>
2022-08-11 00:38:31 +0200

    Music tab

--- src/containers/Main/Main.container.jsx
+++ src/containers/Main/Main.container.jsx
@@ -3,17 +3,18 @@ import React from "react";
 import WeatherApi from "../WeatherAPI/WeatherApi.component";
 import RandomQuote from "../RandomQuote/RandomQuote.component";
 import Hotels from "../Hotels/Hotels.component";
+import Music from "../Music/Music.component";
 
 const Main = () => {
-    return (
-        <div className="main w-full px-20 py-10">
-            <div className="flex">
-                <WeatherApi />
-                <RandomQuote />
-            </div>
-            <Hotels />
-        </div>
-    )
-}
+  return (
+    <div className="main w-full px-20 py-10">
+      <div className="flex">
+        <WeatherApi />
+        <RandomQuote />
+      </div>
+      <Hotels />
+    </div>
+  );
+};
 
 export default Main;
