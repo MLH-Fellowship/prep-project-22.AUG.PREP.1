c39086167e4cc1f0372fb3cb0de6a7bf5b1a2006
Aakanksha Bhende <63249668+aakankshabhende@users.noreply.github.com>
2022-08-11 23:03:17 +0530

    Update MainNavbar.js

--- src/components/MainNavbar.js
+++ src/components/MainNavbar.js
@@ -1,36 +1,56 @@
 import React from 'react'
-
+import logo from '../assets/images/mlh-prep.png'
 const MainNavbar = () => {
   return (
     <>
-      <br />
-      <div class="nav" style={{ backgroundColor: "white" }}>
-        <div class="container">
-          <ul class="pull-left">
-            <li style={{color:'gray'}}>Search Whats on your mind</li>
-            <li>
-              <form>
-                <input
-                  className="search"
-                  type="search"
-                  placeholder="Search..."
-                />
-                <button className="submit" type="submit">
-                  Search
-                </button>
-              </form>
-            </li>
-          </ul>
-
-          <ul class="pull-right">
-            <li>
-              <NavLink to="#">Log In</NavLink>
-            </li>
-          </ul>
-        </div>
+    <div className='h-70 max-w bg-blue-900 align-center mt-0 pt-5'>
+      <img style={{width:"130px"}} src={logo}/>
       </div>
-      <br/><br/>
-    </>
+      <nav class="navbar navbar-expand-xl navbar-light bg-light">
+	<button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+		<span class="navbar-toggler-icon"></span>
+	</button>
+	
+	<div id="navbarCollapse" class="collapse navbar-collapse justify-content-start">
+		<div class="navbar-nav">
+			<a href="#" class="nav-item nav-link disable">SEARCH What's on your mind</a>
+			{/* <a href="#" class="nav-item nav-link">About</a>
+			<div class="nav-item dropdown">
+				<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Services</a>
+				<div class="dropdown-menu">
+					<a href="#" class="dropdown-item">Web Design</a>
+					<a href="#" class="dropdown-item">Web Development</a>
+					<a href="#" class="dropdown-item">Graphic Design</a>
+					<a href="#" class="dropdown-item">Digital Marketing</a>
+				</div>
+			</div>
+			<a href="#" class="nav-item nav-link">Blog</a>
+			<a href="#" class="nav-item nav-link">Contact</a>*/}
+		</div> 
+		<form class="navbar-form form-inline" style={{paddingLeft:"300px"}}>
+			<div class="input-group search-box">								
+				<input type="text" id="search" class="form-control" placeholder="Search"/>
+				<span class="input-group-addon"><i class="material-icons">&#xE8B6;</i></span>
+			</div>
+		</form>
+		<div class="navbar-nav ml-auto">
+			<a href="#" class="nav-item nav-link notifications"><i class="fa fa-bell-o"></i><span class="badge">1</span></a>
+			<a href="#" class="nav-item nav-link messages"><i class="fa fa-envelope-o"></i><span class="badge">10</span></a>
+			<div class="nav-item dropdown">
+				<a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle user-action"><img src="https://www.tutorialrepublic.com/examples/images/avatar/2.jpg" class="avatar" alt="Avatar"/> Paula Wilson <b class="caret"></b></a>
+				<div class="dropdown-menu">
+					<a href="#" class="dropdown-item"><i class="fa fa-user-o"></i> Profile</a>
+					<a href="#" class="dropdown-item"><i class="fa fa-calendar-o"></i> Calendar</a>
+					<a href="#" class="dropdown-item"><i class="fa fa-sliders"></i> Settings</a>
+					<div class="dropdown-divider"></div>
+					<a href="#" class="dropdown-item"><i class="material-icons">&#xE8AC;</i> Logout</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</nav>
+  
+   </>
   )
 }
 
