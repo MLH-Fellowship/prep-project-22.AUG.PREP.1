81b35e2b994b84d24929235ca9244adfbaef7d9f
Nawed Ali <nawed2611@gmail.com>
2022-08-12 14:40:15 +0530

    Add SecNavbar

--- src/components/MainNavbar.js
+++ src/components/MainNavbar.js
@@ -1,41 +1,43 @@
 import React from 'react'
 import logo from '../assets/images/mlh-prep.png'
+import { IoSearchOutline } from 'react-icons/io5'
 const MainNavbar = () => {
-  return (
-    <>
-    <div className='h-70 max-w bg-blue-900 align-center mt-0 pt-5'>
-      <img style={{width:"130px"}} src={logo}/>
-      </div>
-      <nav class="navbar navbar-expand-xl navbar-light bg-light">
-	<button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
-		<span class="navbar-toggler-icon"></span>
-	</button>
-	
-	<div id="navbarCollapse" class="collapse navbar-collapse justify-content-start">
-		<div class="navbar-nav">
-			<a href="#" class="nav-item nav-link disable">SEARCH What's on your mind</a>
-		</div> 
-		<form class="navbar-form form-inline" style={{paddingLeft:"300px"}}>
-			<div class="input-group search-box">								
-				<input type="text" id="search" class="form-control" placeholder="Search"/>
-				<span class="input-group-addon"><i class="material-icons">&#xE8B6;</i></span>
+	return (
+		<div>
+			<div className='h-70 w-full bg-blue-900 flex items-center justify-center p-4'>
+				<img style={{ width: "130px" }} alt="mlh-logo" src={logo} />
 			</div>
-		</form>
-		<div class="navbar-nav ml-auto">
-			<div class="nav-item dropdown">
-				<a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle user-action"><img src="https://www.tutorialrepublic.com/examples/images/avatar/2.jpg" class="avatar" alt="Avatar"/> Paula Wilson <b class="caret"></b></a>
-				<div class="dropdown-menu">
-					<a href="#" class="dropdown-item"><i class="fa fa-user-o"></i> Profile</a>
-					<div class="dropdown-divider"></div>
-					<a href="#" class="dropdown-item"><i class="material-icons">&#xE8AC;</i> Logout</a>
+			<nav class="navbar navbar-expand-xl navbar-light bg-light">
+				<button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+
+				<div id="navbarCollapse" className="flex w-full items-center justify-around "class="collapse navbar-collapse justify-content-start">
+					<div class="navbar-nav">
+						<p class="nav-item nav-link disable">SEARCH What's on your mind</p>
+					</div>
+
+						<form className="flex items-center">
+							<input type="text" id="search" className="border focus:outline-none p-2" placeholder="Start your Search... " />
+							<IoSearchOutline className='mx-2' />
+						</form>
+
+					<div>
+						<div class="nav-item dropdown">
+							<a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle user-action"><img src="https://www.tutorialrepublic.com/examples/images/avatar/2.jpg" class="avatar" alt="Avatar" /> Paula Wilson <b class="caret"></b></a>
+							<div class="dropdown-menu">
+								<a href="#" class="dropdown-item"><i class="fa fa-user-o"></i> Profile</a>
+								<div class="dropdown-divider"></div>
+								<a href="#" class="dropdown-item"><i class="material-icons">&#xE8AC;</i> Logout</a>
+							</div>
+						</div>
+					</div>
+					
 				</div>
-			</div>
+			</nav>
+
 		</div>
-	</div>
-</nav>
-  
-   </>
-  )
+	)
 }
 
 export default MainNavbar
