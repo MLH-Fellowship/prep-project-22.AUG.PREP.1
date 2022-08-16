fadf4233b7d687aca9d84b20bf1cdeb54c59a2f6
Anoushka Jha <95405559+BabyElias@users.noreply.github.com>
2022-08-16 21:00:01 +0530

    Added hotels tab

---
+++ src/containers/Hotels/location.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from "react";
+import HotelApi from "./Hotels.component";
+
+const HotelsLocation = () => {
+    const [latitude, setLatitude] = useState(null)
+    const [longitude, setLongitude] = useState(null)
+
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            setLatitude(position.coords.latitude)
+            setLongitude(position.coords.longitude)
+        })
+    }, [])
+
+
+    return (
+        <div>
+            {(latitude & longitude) && <HotelApi lat={latitude} long={longitude} />}
+        </div>
+    )
+}
+
+
+
+
+export default HotelsLocation;
