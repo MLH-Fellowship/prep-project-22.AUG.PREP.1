59e55ce502f473479bc08d891451d0d8c14e5afb
Suven-p <pandeysuven@gmail.com>
2022-08-18 17:18:27 +0545

    Use bingmaps instead of googlemaps

--- src/containers/Traffic/Traffic.component.jsx
+++ src/containers/Traffic/Traffic.component.jsx
@@ -1,19 +1,23 @@
 import React from "react";
-import { Wrapper } from "@googlemaps/react-wrapper";
-import { useEffect, useRef } from "react";
-import Map from "./Map.component.jsx";
+import { useEffect } from "react";
 import "./Traffic.css";
 
-const render = (status) => {
-  return <h1>{status}</h1>;
-};
-
 const Traffic = ({ zoom, center, apiKey }) => {
+  const Microsoft = window.Microsoft;
+  useEffect(() => {
+    const map = new Microsoft.Maps.Map(document.querySelector('#traffic'), {
+      center: new Microsoft.Maps.Location(center.lat, center.lng),
+      credentials: apiKey,
+      zoom
+    });
+    Microsoft.Maps.loadModule('Microsoft.Maps.Traffic', function () {
+        const manager = new Microsoft.Maps.Traffic.TrafficManager(map);
+        manager.show();
+    });
+  }, [Microsoft, apiKey, center, zoom]);
+
   return (
-    <div className="TrafficAPI w-full bg-gray-300 mt-10 rounded-[10px] h-80 text-center p-20">
-      <Wrapper apiKey={apiKey} render={render}>
-        <Map center={center} zoom={zoom}></Map>
-      </Wrapper>
+    <div className="TrafficAPI w-full bg-gray-300 mt-10 rounded-[10px] h-80 text-center p-20" id="traffic" >
     </div>
   );
 };
